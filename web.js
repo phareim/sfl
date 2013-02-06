a062f2d7722cda2595b82dd607e186e8bcdde1a4
Petter Hareim <phareim@gmail.com>
2013-02-06 04:39:38 -0500

    testing node.js-stuff.

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 
 var app = express.createServer(express.logger());
 
@@ -10,3 +10,43 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  //app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Save For Later'
+  });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
